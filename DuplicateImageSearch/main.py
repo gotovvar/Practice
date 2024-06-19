6ac8e4fb9fc3364d0a1a1a70e8200bb3683be880
Prokopovich Ivan <vanya.prokopovich.02@mail.ru>
2024-06-19 12:38:33 +0300

    add parallel image processing

--- DuplicateImageSearch/main.py
+++ DuplicateImageSearch/main.py
@@ -2,27 +2,37 @@ import os
 import imagehash
 from PIL import Image
 import matplotlib.pyplot as plt
+from concurrent.futures import ProcessPoolExecutor, as_completed
+
+
+def process_image(image_path):
+    try:
+        image = Image.open(image_path)
+        image.verify()
+        image = Image.open(image_path)
+        image_hash = imagehash.phash(image)
+        return image_path, image_hash
+    except (IOError, SyntaxError) as e:
+        print(f"Could not open or verify image file {image_path}: {e}")
+        return image_path, None
 
 
 def find_duplicate_images(folder):
     hashes = {}
     duplicates = []
 
-    for filename in os.listdir(folder):
-        if filename.lower().endswith(('.png', '.jpg', '.jpeg', '.gif', '.bmp')):
-            image_path = os.path.join(folder, filename)
-            try:
-                image = Image.open(image_path)
-                image.verify()
-                image = Image.open(image_path)
-                image_hash = imagehash.phash(image)
+    with ProcessPoolExecutor() as executor:
+        futures = {executor.submit(process_image, os.path.join(folder, filename)): filename
+                   for filename in os.listdir(folder) if
+                   filename.lower().endswith(('.png', '.jpg', '.jpeg', '.gif', '.bmp'))}
 
+        for future in as_completed(futures):
+            image_path, image_hash = future.result()
+            if image_hash:
                 if image_hash in hashes:
                     duplicates.append((image_path, hashes[image_hash]))
                 else:
                     hashes[image_hash] = image_path
-            except (IOError, SyntaxError) as e:
-                print(f"Could not open image file {image_path}: {e}")
 
     return duplicates
 
